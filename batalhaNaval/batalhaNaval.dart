1b9030b68330e64a378331ba19224fff5e273ebe
Bruno Tochetto <draguisada@gmail.com>
2026-03-11 17:01:38 -0300

    Criação do repositório

---
+++ batalhaNaval/batalhaNaval.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+import 'dart:math';
+
+class Ponto {
+  int x;
+  int y;
+  bool navio = false;
+
+  Ponto(this.x, this.y, this.navio) {
+    x = x;
+    y = y;
+    navio = navio;
+  }
+
+}
+
+class Tabuleiro {
+  List<List<Bool>> tabuleiro;
+
+  Tabuleiro({
+    required this.tabuleiro,
+  });
+
+  
+}
+
+// 
+
+
+List<List<String>> tabuleiro = [];
+
+
+
+/*
+casinhas, muitas+
+
+
+ */
